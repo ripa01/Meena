464b15415c4bbef4aca9a786db618634e2b6f4a7
ripa01 <ripabiswas2739@gmail.com>
2025-03-19 00:51:26 +0600

    Add character data fetching and update image configurations

--- meena/src/components/Container.tsx
+++ meena/src/components/Container.tsx
@@ -1,21 +1,10 @@
 import { ReactNode, ElementType, JSX } from "react";
 
-/**
- * Renders a container component that wraps its children with styling and an optional custom element type.
- * @component
- * @param {Object} props - The component props.
- * @param {ElementType} [props.as='div'] - The HTML element type to be used as the container. Defaults to 'div'.
- * @param {ReactNode} props.children - The content to be wrapped inside the container.
- * @param {string} [props.className] - Additional CSS classes to be applied to the container.
- * @param {any} [props.rest] - Any other props to be spread onto the container element.
- * @returns {JSX.Element} The rendered container component.
- */
-
 interface ContainerProps {
   as?: ElementType;
   children: ReactNode;
   className?: string;
-  [key: string]: any; // Allows passing additional props
+  [key: string]: any; 
 }
 
 export const Container = ({
